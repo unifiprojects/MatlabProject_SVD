e8d9dbfaa596fbf62f005c7101ffd9341394a5ec
Matteo Mauro <mauro.matteo96@gmail.com>
2019-12-28 16:30:38 +0100

    Aggiunti commenti

--- aat_svd.m
+++ aat_svd.m
@@ -1,4 +1,6 @@
 function [U, V, sigma] = aat_svd(A)
+    % si costruisce la SVD di AtA usando direttamente gli autovalori ed
+    % autovettori (calcolati per comodita' attraverso eig() )
     num_colonne_A = size(A,2);
     C = A*A';   
     [U, sigma_quad] = eig(C);
@@ -6,10 +8,14 @@ function [U, V, sigma] = aat_svd(A)
     sigma = sqrt(sigma_quad);
 
     V = zeros(num_colonne_A);
-    U = U(:,p);
+    U = U(:,p); %riordino le colonne di U, ovvero gli autovettori
     i=1;
     inversa = pinv(A);
     while i <= length(sigma) && sigma(i) ~= 0
+        % avendo sigma e la matrice U, possiamo ottenere i vettori colonna
+        % di V attraverso la relazione che sussiste A*vi = Sigma*ui, in
+        % particolare utilizziamo la pseudoinversa di A calcolata con
+        % pinv()
         V(:,i) = sigma(i)*inversa*U(:,i); 
         i = i + 1;
     end
--- ata_svd.m
+++ ata_svd.m
@@ -1,4 +1,6 @@
 function [U, V, sigma] = ata_svd(A)
+    % si costruisce la SVD di AtA usando direttamente gli autovalori ed
+    % autovettori (calcolati per comodita' attraverso eig() )
     num_righe_A = size(A,1);
     C = A'*A;   
     [V, sigma_quad] = eig(C);
@@ -6,9 +8,11 @@ function [U, V, sigma] = ata_svd(A)
     sigma = sqrt(sigma_quad);
 
     U = zeros(num_righe_A);
-    V = V(:,p);
+    V = V(:,p); %riordino le colonne di V, ovvero gli autovettori
     i=1;
     while i <= length(sigma) && sigma(i) ~= 0
+        % avendo sigma e la matrice V, possiamo ottenere i vettori colonna
+        % di U attraverso la relazione che sussiste A*vi = Sigma*ui
         U(:,i) = (1/sigma(i))*A*(V(:,i));
         i = i + 1;
     end
